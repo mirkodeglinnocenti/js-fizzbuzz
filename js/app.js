68d8be0f3d61d58ce7c98b24c376886e6d782620
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-02-02 16:05:21 +0100

    FEAT exercise completed added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
-console.log('Ciao Funziona')
+// console.log('Ciao Funziona')
+
+
+// stampare in console i numeri da 1 a 100
+
+for (let i = 1; i <= 100 ;  i++) {
+
+    // console.log(i);
+
+    // RESTO
+    let restoTre = i % 3;
+    let restoCinque = i % 5;
+
+    // per i multipli di entrambi ovvero i multipli di 15 stampare FizzBuzz al posto del numero
+    if (restoTre === 0 && restoCinque === 0){
+
+        console.log('FizzBuzz')
+
+    // per i multipli di 3 stampare Fizz al posto del numero  
+    } else if (restoTre === 0) {
+        console.log('Fizz');
+
+    // per i multipli di 5 stampare Buzz al posto del numero
+    } else if(restoCinque === 0) {
+        console.log('Buzz');
+
+    // stampa tutti gli altri numeri
+    } else {
+        console.log(i)
+    }
+
+
+}
+
+
+
+
